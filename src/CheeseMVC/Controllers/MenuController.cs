41c44a0685e9a63ee20c99e26a83b02637279c7f
Aaron Popp <a_popp@hotmail.com>
2017-04-18 18:43:28 -0500

    Working on Part 3 - stopped at 3rd paragraph of "List Menus"

---
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.Models;
+using System.Collections.Generic;
+using CheeseMVC.ViewModels;
+using CheeseMVC.Data;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace CheeseMVC.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public MenuController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            IList<CheeseMenu> menus = context.CheeseMenus.ToList();
+            // took this out:  Include(c => c.Category)
+
+            return View(menus);
+        }
+    }
+}
